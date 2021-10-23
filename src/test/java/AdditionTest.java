aff4e8c0c3da082522c0372c95042a49237ecd8f
TechSilicon <swati.powar@yahoo.com>
2021-10-23 16:48:48 +0530

    Commit Changes

---
+++ src/test/java/AdditionTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdditionTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+        int result= Addition.add(2,3);
+        int correct=5;
+        assertEquals(5,result);
+    }
+}
